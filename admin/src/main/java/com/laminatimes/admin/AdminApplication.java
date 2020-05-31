9e03b6c41701297dfb7e900df358ca4f2fe29098
SravanPalakala <sravanpalakala@gmail.com>
2020-05-31 19:14:28 +0200

    Added admin module

--- admin/src/main/java/com/laminatimes/admin/AdminApplication.java
+++ admin/src/main/java/com/laminatimes/admin/AdminApplication.java
@@ -1,9 +1,14 @@
 package com.laminatimes.admin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan(basePackages = "com.laminatimes")
+@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages={"com.laminatimes"} ,exclude = { HibernateJpaAutoConfiguration.class})
 public class AdminApplication {
 
 	public static void main(String[] args) {
