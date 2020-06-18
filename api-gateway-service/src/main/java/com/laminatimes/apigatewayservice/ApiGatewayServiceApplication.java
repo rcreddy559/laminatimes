65a119f53466ce91751d947d4e14187fd58baa3b
SravanPalakala <sravanpalakala@gmail.com>
2020-06-18 13:20:27 +0200

    Added Spring cloud gateway

---
+++ api-gateway-service/src/main/java/com/laminatimes/apigatewayservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.laminatimes.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+@EnableHystrix
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ api-gateway-service/src/main/java/com/laminatimes/apigatewayservice/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.laminatimes.apigatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/leaves/**").uri("http://localhost:8086").id("leaves-service"))
+				.build();
+
+	}
+}
+
