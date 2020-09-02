ede47063f23232541e9217f4e8b282a92c23b2d9
Ravichandra Reddy Penumuru <ravichandra.p@outlook.com>
2020-09-02 16:06:01 +0200

    api urls changes

--- api-gateway-service/src/main/java/com/laminatimes/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/laminatimes/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,5 +1,7 @@
 package com.laminatimes.apigatewayservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -8,19 +10,22 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
+	final static Logger logger= LoggerFactory.getLogger(ApiGatewayServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
 	}
 
 	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		logger.info("gatewayRoutes() method");
 		return builder.routes()
-				.route(r -> r.path("/leaves/**").uri("http://localhost:8086").id("leaves-service"))
-				.route(r -> r.path("/authenticate/**").uri("http://localhost:8085").id("admin-service"))
-				.route(r -> r.path("/user/**").uri("http://localhost:8085").id("admin-service"))
+				.route(r -> r.path("/leaves/**").uri("http://localhost:8086"))
+				.route(r -> r.path("/user/**").uri("http://localhost:8084"))
+				.route(r -> r.path("/holiday/**").uri("http://localhost:8088"))
 				.build();
 	}
 }
