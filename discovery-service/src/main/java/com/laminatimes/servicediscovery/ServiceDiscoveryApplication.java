767dda9f75ecfbafb05270a65074a90d070143d3
Ravichandra Reddy Penumuru <ravichandra.p@outlook.com>
2020-09-02 15:51:28 +0200

    Porst Modification

--- discovery-service/src/main/java/com/laminatimes/servicediscovery/ServiceDiscoveryApplication.java
+++ discovery-service/src/main/java/com/laminatimes/servicediscovery/ServiceDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.laminatimes.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaClient
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
