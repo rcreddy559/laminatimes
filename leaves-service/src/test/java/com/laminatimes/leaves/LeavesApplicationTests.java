7193c1906ff180b6b10a772f109d2bbbd7e0fe1c
SravanPalakala <sravanpalakala@gmail.com>
2020-09-25 12:56:59 +0200

    commented annotation

--- leaves-service/src/test/java/com/laminatimes/leaves/LeavesApplicationTests.java
+++ leaves-service/src/test/java/com/laminatimes/leaves/LeavesApplicationTests.java
@@ -3,7 +3,7 @@ package com.laminatimes.leaves;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LeavesApplicationTests {
 
 	@Test
