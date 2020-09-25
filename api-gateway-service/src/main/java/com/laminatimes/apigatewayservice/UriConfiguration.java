9bfbe8701f8cc5a0e9dd060e4b683a17a32fbc51
SravanPalakala <sravanpalakala@gmail.com>
2020-09-25 16:54:35 +0200

    added uriconfig

---
+++ api-gateway-service/src/main/java/com/laminatimes/apigatewayservice/UriConfiguration.java
@@ -0,0 +1,16 @@
+package com.laminatimes.apigatewayservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class UriConfiguration {
+	 private String httpbin = "http://httpbin.org:80";
+
+	  public String getHttpbin() {
+	    return httpbin;
+	  }
+
+	  public void setHttpbin(String httpbin) {
+	    this.httpbin = httpbin;
+	  }
+}
